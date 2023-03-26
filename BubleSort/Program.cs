cfb677121a91567908c10786f3cfd727240d02b0
ahmet bilgic <ahmet_bilgic@yahoo.com>
2023-03-26 18:03:51 +0300

    Add project files.

---
+++ BubleSort/Program.cs
@@ -0,0 +1,18 @@
+﻿//Buble Sort
+var arr = new List<int>() { 2, 5, 3, 8, 6 };
+
+for (int i = 0; i < arr.Count; i++)
+{
+    for (int j = i + 1; j < arr.Count; j++)
+    {
+        if (arr[i] > arr[j])
+        {
+            var temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+    }
+    Console.Write($"{arr[i]} ");
+}
+
